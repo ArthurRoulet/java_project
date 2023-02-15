1614cc7d5a965ea2a8ceffc0f15d061f722a9d91
ArthurRoulet <arthur.roulet@universite-paris-saclay.fr>
2023-02-15 15:33:00 -0500

    test

--- consultationTest.java
+++ consultationTest.java
@@ -1,3 +1,12 @@
+import java.util.Date;
+
 public class consultationTest {
 
+    public static void main(String[] args) {
+        Date date = new Date(2001, 10, 4);
+        consultation test1 = new consultation(date, 10, 30, 30);
+
+        Date date2 = new Date(2001, 10, 4);
+        consultation test2 = new consultation(date, 10, 40, 30);
+    }
 }
