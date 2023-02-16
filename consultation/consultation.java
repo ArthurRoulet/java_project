9a3c33187ce44ff10a3ecf4d32bdf0aeb3efdcf3
ArthurRoulet <arthur.roulet@universite-paris-saclay.fr>
2023-02-16 10:27:39 -0500

    modif

--- consultation/consultation.java
+++ consultation/consultation.java
@@ -7,16 +7,8 @@ public class consultation {
     private Date date;
     private int hour;
     private int minutes;
-    private int duration;
 
     Scanner scan = new Scanner(System.in);
-
-    ArrayList<Date> dateList = new ArrayList<Date>();
-    ArrayList<Integer> hourList = new ArrayList<Integer>();
-    ArrayList<Integer> minutesList = new ArrayList<Integer>();
-    ArrayList<Integer> durationList = new ArrayList<Integer>();
-    ArrayList<Integer> endHourList = new ArrayList<Integer>();
-    ArrayList<Integer> endMinutesList = new ArrayList<Integer>();
 
     // constructor
     // default constructor
@@ -46,15 +38,14 @@ public class consultation {
     } // end of default constructor
 
     // constructor with parameters
-    consultation(Date date, int hour, int minutes, int duration) {
-        String msg = verification(date, hour, minutes, duration);
+    consultation(Date date, int hour, int minutes) throws FileNotFoundException {
+        String msg = verification(date, hour, minutes);
         if (msg == "available") {
             this.date = date;
             this.hour = hour;
             this.minutes = minutes;
-            this.duration = duration;
-            CompletedList(this.date, this.hour, this.minutes, this.duration);
-            System.out.println("consultation confirmed");
+            CompletedList(this.date, this.hour, this.minutes);
+            System.out.println("consultation confirmed at " + this.hour + ":" + this.minutes + " the " + this.date);
         } else {
             System.out.println("the consultation is not available");
             System.out.println("choose a other consultation");
@@ -77,11 +68,6 @@ public class consultation {
         this.minutes = minutes;
     } // end setMinutes() method
 
-    // setDuration
-    public void setDuration(int duration) {
-        this.duration = duration;
-    } // end setDuration() method
-
     // getters
     // getDate
     public Date getDate() {
@@ -98,91 +84,118 @@ public class consultation {
         return this.minutes;
     } // end getMinutes() method
 
-    // getDuration
-    public int getDuration() {
-        return this.duration;
-    } // end getDuration() method
-
     // list
-    public void CompletedList(Date date, int hour, int minutes, int duration) {
+    public void CompletedList(Date date, int hour, int minutes) throws FileNotFoundException {
         int inter_minutes = 0;
         int inter_hour = 0;
+        int day = date.getDay();
+        int month = date.getMonth();
+        int year = date.getYear();
 
-        dateList.add(date);
-        hourList.add(hour);
-        minutesList.add(minutes);
-        durationList.add(duration);
+        Scanner inFile; // instance of the FileReader class
+        PrintWriter outFile; // instance of PrintWriter class
+        inFile = new Scanner(new FileReader("C:\\java_project\\consultation\\consultation.txt"));
+        outFile = new PrintWriter("C:\\java_project\\consultation\\consultation.txt");
 
-        inter_minutes = minutes + duration;
+        inter_minutes = minutes + 30;
         while (inter_minutes > 59) {
             inter_minutes = inter_minutes - 60;
             inter_hour += 1;
         }
         inter_hour += hour;
 
-        endHourList.add(inter_hour);
-        endMinutesList.add(inter_minutes);
+        outFile.println(
+                day + " " + month + " " + year + " " + hour + " " + minutes + " " + inter_hour + " " + inter_minutes);
+
+        // close the inFile and outFile objects
+        inFile.close();
+        outFile.close();
     }
 
     // verification if the slot is available
-    public String verification(Date date, int hour, int minutes, int duration) {
+    public String verification(Date date, int hour, int minutes) throws FileNotFoundException {
         int inter_hour = 0;
         int inter_minute = 0;
-        int inter_duration = 0;
         int end_consultation_hour = 0;
         int end_consultation_minutes = 0;
 
+        Scanner inFile; // instance of the FileReader class
+        PrintWriter outFile; // instance of PrintWriter class
+        inFile = new Scanner(new FileReader("C:\\java_project\\consultation\\consultation.txt"));
+        outFile = new PrintWriter("C:\\java_project\\consultation\\consultation.txt");
+
         String msg = ""; // for the return
 
-        for (int i = 0; i < dateList.size(); i++) {
-            if (dateList.get(i) == date) {
-                if (endHourList.get(i) == hour) {
-                    if (endMinutesList.get(i) <= minutes) {
-                        if ((hourList.get(i + 1) == hour) && (minutesList.get(i + 1) > minutes)) {
-                            end_consultation_minutes = minutes + duration;
+        ArrayList<Date> Date = new ArrayList<Date>(); // date of consultation
+        // hour of consultation
+        ArrayList<Integer> Hour = new ArrayList<Integer>();
+        ArrayList<Integer> Min = new ArrayList<Integer>();
+        // end of consultation
+        ArrayList<Integer> endHour = new ArrayList<Integer>();
+        ArrayList<Integer> endMin = new ArrayList<Integer>();
+
+        // recuperate data of consultation.txt
+        for (int z = 0; z < 200; z++) {
+            int day = inFile.nextInt();
+            int month = inFile.nextInt();
+            int year = inFile.nextInt();
+            Date.add(new Date(year, month, day));
+
+            Hour.add(inFile.nextInt());
+            Min.add(inFile.nextInt());
+            endHour.add(inFile.nextInt());
+            endMin.add(inFile.nextInt());
+        }
+
+        for (int i = 0; i < Date.size(); i++) {
+            if (Date.get(i) == date) {
+                if (endHour.get(i) == hour) {
+                    if (endMin.get(i) <= minutes) {
+                        if ((Hour.get(i + 1) == hour) && (Min.get(i + 1) > minutes)) {
+                            end_consultation_minutes = minutes + 30;
                             end_consultation_hour = hour;
 
                             while (end_consultation_minutes > 59) {
                                 end_consultation_hour += 1;
                                 end_consultation_minutes = end_consultation_minutes - 60;
                             }
-                            if (end_consultation_hour > hourList.get(i + 1)) {
+                            if (end_consultation_hour > Hour.get(i + 1)) {
                                 msg = "not available";
                                 break;
-                            } else if (end_consultation_minutes > minutesList.get(i + 1)) {
+                            } else if (end_consultation_minutes > Min.get(i + 1)) {
                                 msg = "not available";
                                 break;
                             }
-                        } else if (hourList.get(i + 1) > hour) {
-                            end_consultation_minutes = minutes + duration;
+                        } else if (Hour.get(i + 1) > hour) {
+                            end_consultation_minutes = minutes + 30;
                             end_consultation_hour = hour;
 
                             while (end_consultation_minutes > 59) {
                                 end_consultation_hour += 1;
                                 end_consultation_minutes = end_consultation_minutes - 60;
                             }
-                            if (end_consultation_hour > hourList.get(i + 1)) {
+                            if (end_consultation_hour > Hour.get(i + 1)) {
                                 msg = "not available";
                                 break;
-                            } else if (end_consultation_minutes > minutesList.get(i + 1)) {
+                            } else if (end_consultation_minutes > Min.get(i + 1)) {
                                 msg = "not available";
                                 break;
                             }
                         }
                     }
-                } else if ((hourList.get(i + 1) == hour) || (hourList.get(i + 1) == hour + 1)) {
-                    if (minutesList.get(i + 1) >= minutes) {
-                        end_consultation_minutes = minutes + duration;
+                } else if ((Hour.get(i + 1) == hour) || (Hour.get(i + 1) == hour + 1)) {
+                    if (Min.get(i + 1) >= minutes) {
+                        end_consultation_minutes = minutes + 30;
                         end_consultation_hour = hour;
 
                         while (end_consultation_minutes > 59) {
                             end_consultation_hour += 1;
                             end_consultation_minutes = end_consultation_minutes - 60;
                         }
-                        if (end_consultation_hour > hourList.get(i + 1)) {
+                        if (end_consultation_hour > Hour.get(i + 1)) {
                             msg = "not available";
                             break;
-                        } else if (end_consultation_minutes > minutesList.get(i + 1)) {
+                        } else if (end_consultation_minutes > Min.get(i + 1)) {
                             msg = "not available";
                             break;
                         }
@@ -193,6 +206,9 @@ public class consultation {
         if (msg == "") {
             msg = "available";
         }
+        // close the inFile and outFile objects
+        inFile.close();
+        outFile.close();
 
         return msg;
     }
